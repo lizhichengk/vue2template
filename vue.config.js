418a5057da634a2a0dd1c0707905caa5f87088cb
lizhicheng <lizhicheng@zuoyebang.com>
2021-05-08 16:48:59 +0800

    change title

--- vue.config.js
+++ vue.config.js
@@ -3,9 +3,9 @@ const isProduction = process.env.NODE_ENV === 'production';
 module.exports = {
   page: {
     index: {
-      title: {{projectName}}
+      title: '{{projectName}}'
     }
-  }
+  },
   devServer: {
     {{#if proxy}}
     proxy : {
